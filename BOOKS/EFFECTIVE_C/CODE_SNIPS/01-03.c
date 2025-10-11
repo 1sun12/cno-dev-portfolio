982ff29b891b30efd5ac3abe2e95f6c31608b01e
1sun12 <regan_sun12@outlook.com>
2025-10-11 05:56:33 -0400

    swap two integers

---
+++ BOOKS/EFFECTIVE_C/CODE_SNIPS/01-03.c
@@ -0,0 +1,11 @@
+// first time using printf for formatted output
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+	// %s - instructs printf to read the second argument and print in 
+	// \n - new line character
+	printf("%s\n", "Hello, world!");
+	return EXIT_SUCCESS;
+}
