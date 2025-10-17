1fad78e61ef2eb10b0a5a7c537ef6788d07bb48a
1sun12 <regan_sun12@outlook.com>
2025-10-16 23:35:38 -0400

    getting more practice with c-strings and memory allocation

---
+++ PLAYGROUND/c-stwing.c
@@ -0,0 +1,11 @@
+// I LOVE C STRINGS!!! :do {
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	char *word = (char *)calloc(6, sizeof(char)); // allocate 6 bytes
+	
+
+
+	return EXIT_SUCCESS;
+}
