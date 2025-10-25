0e5da133ae7d0da203c2cc63ae4c4f1725c2d49a
1sun12 <regan_sun12@outlook.com>
2025-10-25 00:11:57 -0400

    dynamic mem copy of 'h' into 4 byte allocation

--- PLAYGROUND/01-char-array-shenanigans.c
+++ PLAYGROUND/01-char-array-shenanigans.c
@@ -2,9 +2,13 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <cstring>
 
 int main(void) {
 	
 	char *c_str = (char *)calloc(2, sizeof(char));
+	strcpy(c_str, "h");
+	printf("%s", c_str);
+
 	return EXIT_SUCCESS;
 }
