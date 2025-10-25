a6c5f61f83f5ddee6139151ebf2ff7212535dd60
1sun12 <regan_sun12@outlook.com>
2025-10-24 23:51:10 -0400

    made a quick makefile

--- PLAYGROUND/01-char-array-shenanigans.c
+++ PLAYGROUND/01-char-array-shenanigans.c
@@ -1,3 +1,10 @@
 // learning how to use calloc and malloc for c strings
 
-a
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	
+	char *c_str = (char *)calloc(2, sizeof(char));
+	return EXIT_SUCCESS;
+}
