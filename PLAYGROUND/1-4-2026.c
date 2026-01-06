d8e5dfd1b2be9dfe1231f26aafbd8e68d68da3ea
1sun12 <regan_sun12@outlook.com>
2026-01-05 22:37:07 -0500

    1-5-2026

---
+++ PLAYGROUND/1-4-2026.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <cstring>
+
+int main(void) {
+    printf("Type a sentence to be reversed");
+
+    // obtain line and place in memory, sizeof input
+    char *sentence = NULL;
+
+    while (true) {
+
+    }
+    
+}
