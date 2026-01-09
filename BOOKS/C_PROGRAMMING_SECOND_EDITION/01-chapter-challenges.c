a2eb8138aebe859250de69dc5389c5002d3b94f2
1sun12 <regan_sun12@outlook.com>
2026-01-08 20:30:53 -0500

    chapter 1 challenges

---
+++ BOOKS/C_PROGRAMMING_SECOND_EDITION/01-chapter-challenges.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(void){
+    // 1. study the vim quick guide (done)
+    // 2. study the nano quick guide (done)
+    // 3. create a program that prints your name
+    printf("\nName: sun!");
+
+    //4. create a program that uses escape sequence \ " to print a quote
+    printf("\n\"this is a quote\"");
+
+    //5. use \\ to print a directory name
+    printf("\nC:\\directory\\data\\folder\\blah.c");
+
+    //6. write a program that prints a diamond as demonstrated next
+    printf("\n\nDIAMOND!!\n\n");
+    printf("\n\t\t\t*");
+    printf("\n\t\t*\t\t*");
+    printf("\n\t*\t\t\t\t*");
+    printf("\n*\t\t\t\t\t\t*");
+    printf("\n\t*\t\t\t\t*");
+    printf("\n\t\t*\t\t*");
+    printf("\n\t\t\t*");
+    printf("\n\nDIAMOND!\n\n");
+
+
+    return EXIT_SUCCESS;
+}
